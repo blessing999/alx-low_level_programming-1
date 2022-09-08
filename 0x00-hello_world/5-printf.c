cee199e21990399ec2e763d40b72757e997bbff0
OB-Peter <obpeterapp@gmail.com>
2022-09-08 04:58:19 -0700

    All the project Files

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  *
  *  * main - Entry point
@@ -14,8 +13,8 @@ int main(void)
 
 {
 
-	printf("with proper grammar, but the outcome is a piece of art,\n");
+		printf("with proper grammar, but the outcome is a piece of art,\n");
 
-	return (0);
+			return (0);
 
 }
