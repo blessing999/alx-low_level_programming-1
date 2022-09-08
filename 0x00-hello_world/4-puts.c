5206f0a3ffbe5986f4787abc09dc67503d73492b
OB-Peter <obpeterapp@gmail.com>
2022-09-08 04:55:13 -0700

    My Fourth commir

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+
+
 /**
  *
  *  * main - Entry point
@@ -14,8 +16,8 @@ int main(void)
 
 {
 
-	printf("with proper grammar, but the outcome is a piece of art,\n");
+		puts("\"Programming is like building a multilingual puzzle");
 
-	return (0);
+			return (0);
 
 }
